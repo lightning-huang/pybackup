b999e55759c2e4e6aa8eb9e647c22a90c1e5243d
轩辕隐天山 <16414556@qq.com>
2017-06-24 22:12:54 +0800

    put things to parameters

--- backupconfig.py
+++ backupconfig.py
@@ -2,5 +2,8 @@ destfolder=u"h:\\baiduyundownload"
 hashfile=u"h:\\baidyunindex.tsv"
 srcfolder=u"F:\\BaiduYunDownload"
 maxdays=60
+picked_size=1024000
+codec_name="gb18030"
+file_ignore_suffix="baiduyun.downloading"
 
 indexfile=hashfile
